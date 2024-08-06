6c847f4ef322340e65c3ea99911b7aecbf93be6b
rki28 <101640464+rki28@users.noreply.github.com>
2024-08-06 10:43:10 -0400

    Update server.js to import websocket.js functionality

--- app/server.js
+++ app/server.js
@@ -1,5 +1,13 @@
-var express = require("express");
-var app = express();
+const express = require("express");
+const app = express();
+const expressWs = require("express-ws")(app);
+
+const port = 3000;
+const hostname = "localhost";
+
+app.use(express.static("public"));
+
+require("./websocket")(app);
 
 app.set("view engine", "ejs");
 
@@ -7,5 +15,7 @@ app.get("/", function (req, res) {
 	res.render("pages/home");
 });
 
-app.listen(8080);
-console.log("Server is listening on port 8080");
+app.listen(port, hostname, () => {
+	console.log(`Listening at: http://${hostname}:${port}`);
+	console.log(`Websocket server running on: ws://${hostname}:${port}/echo`);
+});
--- app/websocket.js
+++ app/websocket.js
@@ -1,41 +1,29 @@
-const express = require("express");
-const app = express();
-const expressWs = require("express-ws")(app);
+module.exports = (app) => {
+	// defines middleware for every incoming request
+	app.use((req, res, next) => {
+		// adds a property called "testing" to the request object, making it available in any subsequent handler
+		// currently testing with just the url
+		req.testing = `you're on ${req.url}`;
 
-const port = 3000;
-const hostname = "localhost";
-
-app.use(express.static("public"));
-
-// defines middleware for every incoming request
-app.use((req, res, next) => {
-	// adds a property called "testing" to the request object, making it available in any subsequent handler
-	// currently testing with just the url
-	req.testing = `you're on ${req.url}`;
-
-	// makes server continue processing the request by moving on to the next handler
-	return next();
-});
-
-app.get("/hello", (req, res, next) => {
-	console.log("get route", req.testing);
-
-	res.send(req.testing).end();
-});
-
-app.ws("/echo", (ws, req) => {
-	ws.on("message", (msg) => {
-		console.log(`Received message: ${msg}`);
-		// Echo the message back to the client
-		ws.send(`Server received: ${msg}`);
+		// makes server continue processing the request by moving on to the next handler
+		return next();
 	});
 
-	ws.on("close", () => {
-		console.log("Client disconnected");
-	});
-});
+	app.get("/hello", (req, res, next) => {
+		console.log("get route", req.testing);
 
-app.listen(port, hostname, () => {
-	console.log(`Websocket server running on: ws://${hostname}:${port}/echo`);
-	console.log(`Listening at: http://${hostname}:${port}`);
-});
+		res.send(req.testing).end();
+	});
+
+	app.ws("/echo", (ws, req) => {
+		ws.on("message", (msg) => {
+			console.log(`Received message: ${msg}`);
+			// Echo the message back to the client
+			ws.send(`Server received: ${msg}`);
+		});
+
+		ws.on("close", () => {
+			console.log("Client disconnected");
+		});
+	});
+};
