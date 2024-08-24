ec5265981b69d2115a7499075b69102831822b85
Wilber567 <131905509+Wilber567@users.noreply.github.com>
2024-08-24 01:16:09 -0400

    switched game to socket.io

--- app/websocket.js
+++ app/websocket.js
@@ -10,6 +10,9 @@ let host = "localhost";
 let port = 3000;
 
 app.use(express.static("public"));
+app.use(express.json());
+
+let blackjack = require('./public/blackjack');
 
 // global object to hold socket objects; structure is:
 // {[roomId]: {[socketId]: [...socket objects...]}}
@@ -72,7 +75,24 @@ app.get("/room/:roomId", (req, res) => {
 // if you need to do things like associate a socket with a logged in user, see
 // https://socket.io/how-to/deal-with-cookies
 // to see how you can fetch application cookies from the socket
+let turn = "";
 
+async function handlePlayerTurns(players, deck) {
+	for (const playerKey of Object.keys(players)) {
+	  const player = players[playerKey];
+  
+	  console.log(player);
+  
+	  turn = playerKey; // Set the current player's turn
+  
+	  // Wait for 30 seconds before proceeding to the next player
+	  await new Promise(resolve => setTimeout(() => {
+		turn = ""; 
+		resolve();
+	  }, 10000));
+	}
+  }
+  
 io.on("connection", (socket) => {
 	console.log(`Socket ${socket.id} connected`);
 
@@ -92,6 +112,20 @@ io.on("connection", (socket) => {
 
 	// add socket object to room so other sockets in same room can send messages to it later
 	rooms[roomId][socket.id] = socket;
+
+	//initialize in lobby
+	if (!rooms[roomId].hasOwnProperty('game')){
+		rooms[roomId].game = {dealer: blackjack.initDealer(), players: {"me": blackjack.initPlayer("me", 500, socket.id)}, deck: blackjack.shuffle(1)};
+	} else {
+		rooms[roomId].game.players["you"] = blackjack.initPlayer("you", 500, socket.id);
+	}
+	//console.log(JSON.stringify(rooms[roomId].game));
+
+	let players = rooms[roomId].game.players;
+	let deck = rooms[roomId].game.deck;
+	let dealer = rooms[roomId].game.dealer;
+
+
 
 	/* MUST REGISTER socket.on(event) listener FOR EVERY event CLIENT CAN SEND */
 
@@ -114,6 +148,8 @@ io.on("connection", (socket) => {
 			socketId: socket.id,
 			message,
 		};
+		console.log(message);
+		
 
 		// this would send the message to all other sockets
 		// but we want to only send it to other sockets in this room
@@ -128,6 +164,79 @@ io.on("connection", (socket) => {
 			otherSocket.emit("relay", messageData);
 		}
 	});
+
+	socket.on("playerReady" ,(message) => {
+		//message [0] = id, message[1] = name
+		let ready = true;
+		players[message[1]].status= "ready";
+		players[message[1]].id= message[0];
+
+		Object.keys(players).forEach(player => {
+			if (players[player].status !== "ready") ready = false;
+		  });
+
+		if(ready){
+			dealer = blackjack.dealerHand(dealer, deck);
+			Object.keys(players).forEach(player => {
+				blackjack.newHand(players[player], deck);
+				});
+			handlePlayerTurns(players, deck).then(() => {
+				console.log('All players have had their turn.');
+			});
+
+			for (let sockets of Object.values(rooms[roomId])) {
+				console.log(`Sending message ${message} to socket ${sockets.id}`);
+				if(sockets.emit) sockets.emit("playerData", [players, dealer]);
+			}
+		}
+	});
+	socket.on("askForCard" ,(message) => {
+		let socketId = message[0]
+		io.to(socketId).emit("card", deck.pop());
+	});
+	socket.on("bet" ,(message) => {
+		let bet = message[0];
+		let playerName = message[1];
+		players[playerName].balance -= bet;
+		players[playerName].bet += bet;
+		console.log(players);
+	});
+	socket.on("win" ,(message) => {
+		let win = message[0];
+		let playerName = message[1];
+		players[playerName].balance += win;
+		players[playerName].win += win;
+		console.log(players);
+	});	
+	socket.on("hand" ,(message) => {
+		let playerName = message[0];
+		let cards = message[1];
+		players[playerName].cards = cards;
+		console.log(cards);
+	});
+	socket.on("done" ,(message) => {
+		let done = true;
+		let playerName = message[1];
+		console.log(players[playerName]);
+		players[playerName].status = "done";
+		Object.keys(players).forEach(player => {
+			if (players[player].status !== "done") done = false;
+		  });
+		if(done){
+			blackjack.endDealer(dealer, deck);
+
+			for (let sockets of Object.values(rooms[roomId])) {
+				if(sockets.emit) sockets.emit("end", dealer);
+			}
+
+			rooms[roomId].game.dealer = new blackjack.initDealer(deck);
+				Object.keys(players).forEach(player => {
+				curBalance = players[player].balance;
+				console.log(players[player].cards);
+				players[player] = blackjack.initPlayer("me", curBalance, message[0]);
+			});
+		}
+		  });
 });
 
 server.listen(port, host, () => {
